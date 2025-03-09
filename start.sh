0292910d3fa3345432b189fc23b73474a854d6ff
AGSQ <67051233+AGSQ11@users.noreply.github.com>
2025-03-09 13:53:37 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set environment variables
+export JACKETT_API_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxx
+export JACKETT_URL=http://0.0.0.0:9117
+export SERVER_IP=0.0.0.0  # Set to your desired IP address
+
+# Start the application
+npm start
